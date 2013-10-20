cb8ca91473586c3cb2a843a129935de23d96bb76
Chris <chris@chris-alexander.co.uk>
2013-10-20 12:37:29 +0000

    Initial python

---
+++ amoebatwo_bounce.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import amoebatwo
+
+m = amoebatwo.AmoebaTwo()
+
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name='amoebatwo_bounce',
+    version='0.1',
+    description='AmoebaTwo Bounce test',
+    author='Chris Alexander',
+    author_email='chris@chris-alexander.co.uk',
+    license='MIT',
+    url='https://github.com/chrisalexander/AmoebaTwo_Bounce',
+    py_modules=['amoebatwo_bounce'],
+    requires=['amoebatwo'],
+)
