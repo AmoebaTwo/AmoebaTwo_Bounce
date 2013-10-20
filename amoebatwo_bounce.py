85ddb033dea68b97ee7a841dcdfe20adc8728d61
Chris <chris@chris-alexander.co.uk>
2013-10-20 13:47:59 +0000

    Bounce implementation

--- amoebatwo_bounce.py
+++ amoebatwo_bounce.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 import amoebatwo
+import time
+import sys
 
 m = amoebatwo.AmoebaTwo()
 
+triggered = False;
+
+def stop(event):
+	m.move.stop();
+	triggered = True;
+
+m.registerListener(0, stop);
+m.registerListener(1, stop);
+
+m.move.forwards();
 
